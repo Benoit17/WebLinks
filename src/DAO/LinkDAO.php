fcbc825f4517ef8b43cd7414bbc7df0c1c7a5eb8
Benoît <Benot>
2017-03-28 16:55:36 +0200

    Modification de LinkDAO à l'aide de BookDAO

--- src/DAO/LinkDAO.php
+++ src/DAO/LinkDAO.php
@@ -4,8 +4,17 @@ namespace WebLinks\DAO;
 
 use WebLinks\Domain\Link;
 
-class LinkDAO extends DAO 
+class LinkDAO extends DAO
 {
+    /**
+     * @var \WebLinks\DAO\UserDAO
+     */
+    private $userDAO;
+
+    public function setUserDAO(UserDAO $userDAO) {
+        $this->userDAO = $userDAO;
+    }
+    
     /**
      * Returns a list of all links, sorted by id.
      *
@@ -16,12 +25,29 @@ class LinkDAO extends DAO
         $result = $this->getDb()->fetchAll($sql);
         
         // Convert query result to an array of domain objects
-        $entities = array();
+        $links = array();
         foreach ($result as $row) {
             $id = $row['link_id'];
-            $entities[$id] = $this->buildDomainObject($row);
+            $links[$id] = $this->buildDomainObject($row);
         }
-        return $entities;
+        return $links;
+    }
+
+    /**
+     * Returns a link matching the supplied id.
+     *
+     * @param integer $id The link id.
+     *
+     * @return \WebLinks\Domain\Link|throws an exception if no matching book is found
+     */
+    public function find($id) {
+        $sql = "select * from t-link where link_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No link matching id " . $id);
     }
 
     /**
@@ -35,6 +61,13 @@ class LinkDAO extends DAO
         $link->setId($row['link_id']);
         $link->setUrl($row['link_title']);
         $link->setTitle($row['link_url']);
+
+        if (array_key_exists('user_id', $row)) {
+            // Find and set the associated author
+            $userId = $row['user_id'];
+            $author = $this->userDAO->find($userId);
+            $link->setAuthor($author);
+        }
         
         return $link;
     }
